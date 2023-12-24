5828475cebd67d34fafa3c0930cdf7235312e3c7
Charles Russell <chjrussell@gmail.com>
2023-12-24 12:58:45 -0700

    Add Analytics component to RootLayout

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <SpeedInsights />
+      <Analytics />
       <body className="bg-background text-text font-inter">{children}</body>
     </html>
   );
