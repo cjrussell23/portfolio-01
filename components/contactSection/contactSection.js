16d8a3f77c7743b750932e43c0a2c293db451507
GetMoby <ap@getmoby.com>
2024-01-17 09:43:42 -0700

    Update contact section styling

--- components/contactSection/contactSection.js
+++ components/contactSection/contactSection.js
@@ -20,7 +20,7 @@ export default function ContactSection() {
       <p className="tw-mb-8 tw-w-full tw-text-xl">
         {`Feel free to reach out to me and connect!`}
       </p>
-      <div className="tw-mb-8 tw-flex tw-w-full tw-flex-row tw-items-center tw-justify-start tw-gap-8 tw-text-2xl">
+      <div className="tw-mb-8 tw-flex tw-w-full tw-flex-row tw-flex-wrap tw-items-center tw-justify-start tw-gap-8 tw-text-2xl">
         {Socials.map((link, index) => (
           <Link
             href={link.href}
