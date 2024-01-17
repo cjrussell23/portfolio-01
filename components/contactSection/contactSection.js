ac1041daa24cac02448e19b2df2a3e323e59a7e9
GetMoby <ap@getmoby.com>
2024-01-17 09:35:40 -0700

    Fix string error

--- components/contactSection/contactSection.js
+++ components/contactSection/contactSection.js
@@ -11,14 +11,14 @@ export default function ContactSection() {
     >
       <h2 className="tw-mb-8 tw-w-full tw-text-4xl tw-font-bold">Contact</h2>
       <p className="tw-mb-8 tw-w-full tw-text-xl">
-        I'm always open to new opportunities and would love to hear from you!
+        {`I'm always open to new opportunities and would love to hear from you!`}
       </p>
       <div className="tw-mb-20 tw-w-full tw-rounded-md tw-border tw-border-muted-foreground tw-p-6">
         <ContactForm />
       </div>
       <h2 className="tw-mb-8 tw-w-full tw-text-4xl tw-font-bold">Socials</h2>
       <p className="tw-mb-8 tw-w-full tw-text-xl">
-        Feel free to reach out to me and connect!
+        {`Feel free to reach out to me and connect!`}
       </p>
       <div className="tw-mb-8 tw-flex tw-w-full tw-flex-row tw-items-center tw-justify-start tw-gap-8 tw-text-2xl">
         {Socials.map((link, index) => (
