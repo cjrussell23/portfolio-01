8431189f38ab27c3699a3f65106a80529fa142c3
Charles Russell <cj.russell2396@gmail.com>
2024-02-25 20:54:12 -0700

    add download link on mobile

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,6 +1,8 @@
 "use client";
 import Resume from "./resume";
 import dynamic from "next/dynamic";
+import { PDFDownloadLink } from "@react-pdf/renderer";
+import { isMobile, isBrowser } from "react-device-detect";
 
 const PDFViewer = dynamic(
   () => import("@react-pdf/renderer").then((mod) => mod.PDFViewer),
@@ -23,6 +25,15 @@ const PDFViewer = dynamic(
 );
 
 export default function ResumePage() {
+  if (isMobile) {
+    return (
+      <PDFDownloadLink document={<Resume />} fileName="resume.pdf">
+        {({ blob, url, loading, error }) =>
+          loading ? "Loading document..." : "Download now!"
+        }
+      </PDFDownloadLink>
+    );
+  }
   return (
     <PDFViewer style={{ width: "100%", height: "100vh" }}>
       <Resume />
